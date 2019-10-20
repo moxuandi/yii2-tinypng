6f10f9d992dee4c41a6c2ecca61ae7d12c39fb18
moxuandi <1104984259@qq.com>
2019-10-20 09:41:05 +0800

    异常提示

--- TinyImage.php
+++ TinyImage.php
@@ -1,10 +1,16 @@
 <?php
 namespace moxuandi\tinypng;
 
+use Tinify\AccountException;
+use Tinify\ClientException;
+use Tinify\ConnectionException;
+use Tinify\Exception;
+use Tinify\ServerException;
 use Tinify\Source;
 use Tinify\Tinify;
 use Yii;
 use yii\helpers\FileHelper;
+
 /**
  * Class ArrayHelper 拓展数组助手类
  *
@@ -29,24 +35,36 @@ class TinyImage
      * 申请地址: https://tinypng.com/developers
      */
     private $apiKeys = [];
+    /**
+     * @var int key 的序号
+     */
     private $num = 0;
+    /**
+     * @var string 存储 key 序号的缓存键.
+     */
+    private $_key = 'tiny_num';
 
-
-    function __construct()
+    /**
+     * TinyImage constructor.
+     */
+    public function __construct()
     {
         $this->apiKeys = Yii::$app->params['tinyPngApiKeys'];
+        if(Yii::$app->cache && ($num = Yii::$app->cache->get($this->_key))){
+            $this->num = $num;
+        }
     }
 
     /**
      * 压缩目录中的所有图像.
      * @param string $inputDir 输入目录.
      * @param string $outputDir 输出目录. 设置为空字符串时, 将使用输入目录, 这将覆盖原图.
-     * @param array $findOptions 文件搜索选项. 参考`FileHelper::findFiles()`方法的`$options`属性.
      * @param array|bool $resize 调整图像大小的配置. 参考`compressImage()`方法的`$resize`属性.
+     * @param array $findOptions 文件搜索选项. 参考`FileHelper::findFiles()`方法的`$options`属性.
      * @return array
      * @throws yii\base\Exception
      */
-    public function compressFile($inputDir, $outputDir = '', $findOptions = [], $resize = false)
+    public function compressFile($inputDir, $outputDir = '', $resize = false, $findOptions = [])
     {
         $outputDir = $outputDir ? $outputDir : $inputDir;
         $files = FileHelper::findFiles($inputDir, $findOptions);
@@ -131,18 +149,52 @@ class TinyImage
      *  - `toFile($path)`: 保存图片到指定位置
      *  - 'store($options)': 将图像保存到`Amazon S3`或`Google Cloud Storage`.
      *  - 'resize($options)': 调整图像大小. 参考`compressImage()`方法的`$resize`属性.
+     * @throws ClientException
+     * @throws ConnectionException
+     * @throws Exception
+     * @throws ServerException
      */
     public function getSource($input, $type = 'file')
     {
-        $key = $this->apiKeys[$this->num];
-        Tinify::setKey($key);
-        file_put_contents("tiny-count.txt", date('Y-m-d H:i:s') . " key`{$key}`使用次数: " . Tinify::getCompressionCount() . "\n", FILE_APPEND);
-        switch($type){
-            case 'buffer': $source = Source::fromBuffer($input); break;
-            case 'url': $source = Source::fromUrl($input); break;
-            case 'file':
-            default: $source = Source::fromFile($input); break;
+        try{
+            $key = $this->apiKeys[$this->num];
+            Tinify::setKey($key);
+            switch($type){
+                case 'buffer': $source = Source::fromBuffer($input); break;
+                case 'url': $source = Source::fromUrl($input); break;
+                case 'file':
+                default: $source = Source::fromFile($input); break;
+            }
+            file_put_contents(Yii::getAlias('@runtime/tiny-logs.txt'), date('Y-m-d H:i:s') . " 使用`{$key}`压缩文件`{$input}`结果: " . var_export($source, true) . "\n\n", FILE_APPEND);
+            return $source;
+        }catch(AccountException $e){
+            file_put_contents(Yii::getAlias('@runtime/tiny-count.txt'), date('Y-m-d H:i:s') . " The key `{$key}` monthly limit has been exceeded.\n", FILE_APPEND);
+            if($e->status == 429){
+                $this->num += 1;
+                if($this->num < count($this->apiKeys)){
+                    Yii::$app->cache->set($this->_key, $this->num);
+                    return self::getSource($input, $type);
+                }else{
+                    throw new AccountException("All keys are limit has been exceeded, No key available.");
+                }
+            }
+        }catch(ClientException $e){
+            throw new ClientException($e->getMessage(), null, $e->status);
+        }catch(ServerException $e){
+            throw new ServerException($e->getMessage(), null, $e->status);
+        }catch(ConnectionException $e){
+            throw new ConnectionException($e->getMessage(), null, $e->status);
+        }catch(Exception $e){
+            throw new Exception($e->getMessage(), null, $e->status);
         }
-        return $source;
+    }
+
+    /**
+     * 析构函数.
+     */
+    public function __destruct()
+    {
+        $key = $this->apiKeys[$this->num];
+        file_put_contents(Yii::getAlias('@runtime/tiny-count.txt'), date('Y-m-d H:i:s') . " key`{$key}`使用次数: " . Tinify::getCompressionCount() . "\n", FILE_APPEND);
     }
 }
